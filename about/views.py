392f2b5d1a20e8aed5a2b4a3a3ee2eaef2b08802
Lane Moseley <33134654+lanemoseley@users.noreply.github.com>
2021-03-12 23:10:19 -0700

    Markdown support for the About page.

--- about/views.py
+++ about/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from .models import Post
+import markdown
 
 
 def about_detail(request):
+    # If there are multiple "about me" posts, just get the most recent one
     first = Post.objects.all().order_by('-created_on').first()
     if first is not None:
         pk = first.pk
         post = Post.objects.get(pk=pk)
+        post.body = markdown.markdown(post.body,
+                                extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.fenced_code',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                ])
+
     else:
         post = Post()
         post.title = "Whoops! Something went wrong..."
