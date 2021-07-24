e3fb7d5c1057b5ed33d1181e6c494ba10c218742
Lane Moseley <moseley.lane@gmail.com>
2021-07-24 12:16:38 -0600

    Remove psycopg2 from requirements. Having psycopg2-binary is all that is needed.

--- anthem_3/urls.py
+++ anthem_3/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('', include('projects.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
