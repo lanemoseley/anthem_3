e074d78cc1985a64a87b0ab7e10804d5b8fcd011
Lane Moseley <moseley.lane@gmail.com>
2025-01-29 23:09:38 -0500

    Adding allowed hosts.

--- anthem_3/settings.py
+++ anthem_3/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.56.112.34', 'lanemoseley.dev', 'lanemoseley.com']
 
 # Application definition
 
