6411faf869034e6e69b92e3c48cef93a9da35407
Lane Moseley <moseley.lane@gmail.com>
2025-02-03 23:56:34 -0500

    Add created & modified dates to projects.

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,8 @@ class Project(models.Model):
     technology = models.CharField(max_length=100)
     url = models.URLField(blank=True, null=True, max_length=250)
     image = models.ImageField(upload_to="images/")
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title}"
